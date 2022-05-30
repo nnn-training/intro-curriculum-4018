73c5183b23fc51694a25bb54d61aa1c533d2569c
YURI <taro0koma@gmail.com>
2022-05-30 15:20:41 +0900

    作成済み予定と候補の表示テストを実装

--- test/test.js
+++ test/test.js
@@ -7,82 +7,86 @@ const Schedule = require('../models/schedule');
 const Candidate = require('../models/candidate');
 
 describe('/login', () => {
-  beforeAll(() => {
-    passportStub.install(app);
-    passportStub.login({ username: 'testuser' });
-  });
+    beforeAll(() => {
+        passportStub.install(app);
+        passportStub.login({ username: 'testuser' });
+    });
 
-  afterAll(() => {
-    passportStub.logout();
-    passportStub.uninstall(app);
-  });
+    afterAll(() => {
+        passportStub.logout();
+        passportStub.uninstall(app);
+    });
 
-  test('ログインのためのリンクが含まれる', async () => {
-    await request(app)
-      .get('/login')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(/<a href="\/auth\/github"/)
-      .expect(200);
-  });
+    test('ログインのためのリンクが含まれる', async() => {
+        await request(app)
+            .get('/login')
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .expect(/<a href="\/auth\/github"/)
+            .expect(200);
+    });
 
-  test('ログイン時はユーザー名が表示される', async () => {
-    await request(app)
-      .get('/login')
-      .expect(/testuser/)
-      .expect(200);
-  });
+    test('ログイン時はユーザー名が表示される', async() => {
+        await request(app)
+            .get('/login')
+            .expect(/testuser/)
+            .expect(200);
+    });
 });
 
 describe('/logout', () => {
-  test('/ にリダイレクトされる', async () => {
-    await request(app)
-      .get('/logout')
-      .expect('Location', '/')
-      .expect(302);
-  });
+    test('/ にリダイレクトされる', async() => {
+        await request(app)
+            .get('/logout')
+            .expect('Location', '/')
+            .expect(302);
+    });
 });
 
 describe('/schedules', () => {
-  let scheduleId = '';
-  beforeAll(() => {
-    passportStub.install(app);
-    passportStub.login({ id: 0, username: 'testuser' });
-  });
-
-  afterAll(async () => {
-    passportStub.logout();
-    passportStub.uninstall(app);
-
-    // テストで作成したデータを削除
-    const candidates = await Candidate.findAll({
-      where: { scheduleId: scheduleId }
+    let scheduleId = '';
+    beforeAll(() => {
+        passportStub.install(app);
+        passportStub.login({ id: 0, username: 'testuser' });
     });
-    const promises = candidates.map((c) => {
-      return c.destroy();
+
+    afterAll(async() => {
+        passportStub.logout();
+        passportStub.uninstall(app);
+
+        // テストで作成したデータを削除
+        const candidates = await Candidate.findAll({
+            where: { scheduleId: scheduleId }
+        });
+        const promises = candidates.map((c) => {
+            return c.destroy();
+        });
+        await Promise.all(promises)
+        const s = await Schedule.findByPk(scheduleId)
+        await s.destroy()
     });
-    await Promise.all(promises)
-    const s = await Schedule.findByPk(scheduleId)
-    await s.destroy()
-  });
 
-  test('予定が作成でき、表示される', async () => {
-    await User.upsert({ userId: 0, username: 'testuser' });
-    const res = await request(app)
-      .post('/schedules')
-      .send({
-        scheduleName: 'テスト予定1',
-        memo: 'テストメモ1\r\nテストメモ2',
-        candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3'
-      })
-      .expect('Location', /schedules/)
-      .expect(302)
+    test('予定が作成でき、表示される', async() => {
+        await User.upsert({ userId: 0, username: 'testuser' });
+        const res = await request(app)
+            .post('/schedules')
+            .send({
+                scheduleName: 'テスト予定1',
+                memo: 'テストメモ1\r\nテストメモ2',
+                candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3'
+            })
+            .expect('Location', /schedules/)
+            .expect(302)
 
-    const createdSchedulePath = res.headers.location;
-    scheduleId = createdSchedulePath.split('/schedules/')[1];
-    await request(app)
-      .get(createdSchedulePath)
-      // TODO 作成された予定と候補が表示されていることをテストする
-      .expect(200)
-  });
+        const createdSchedulePath = res.headers.location;
+        scheduleId = createdSchedulePath.split('/schedules/')[1];
+        await request(app)
+            .get(createdSchedulePath)
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)
+            .expect(200)
+    });
 });
-
